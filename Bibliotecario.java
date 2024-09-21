cfd08236379d2e9acc8d454a917222ecbe9cff09
Santiago-Bayona <Cesars.bayonap@uqvirtual.edu.co>
2024-09-21 18:51:17 -0500

    Bibliotecario.java

--- Bibliotecario.java
+++ Bibliotecario.java
@@ -1,53 +1,15 @@
 package Biblioteca;
 
-public class Bibliotecario {
-    private String nombre;
-    private String cedula;
-    private String telefono;
-    private String Correo;
+public class Bibliotecario extends Persona {
     private int Salario;
     private int antiguedad;
 
-    public Bibliotecario(String nombre,String cedula, String telefono,String correo,int salario,int antiguedad) {
-        this.nombre=nombre;
-        this.cedula=cedula;
-        this.telefono=telefono;
-        this.Correo=correo;
-        this.Salario=salario;
-        this.antiguedad=antiguedad;
+    public Bibliotecario(String nombre, String cedula, String telefono, String correo, int salario, int antiguedad) {
+        super(nombre, cedula, telefono, correo);
+        this.Salario = salario;
+        this.antiguedad = antiguedad;
     }
 
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getCedula() {
-        return cedula;
-    }
-
-    public void setCedula(String cedula) {
-        this.cedula = cedula;
-    }
-
-    public String getTelefono() {
-        return telefono;
-    }
-
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
-    }
-
-    public String getCorreo() {
-        return Correo;
-    }
-
-    public void setCorreo(String correo) {
-        Correo = correo;
-    }
 
     public int getSalario() {
         return Salario;
@@ -67,11 +29,7 @@ public class Bibliotecario {
 
     @Override
     public String toString() {
-        return "Bibliotecario{" +
-                "nombre='" + nombre + '\'' +
-                ", cedula='" + cedula + '\'' +
-                ", telefono='" + telefono + '\'' +
-                ", Correo='" + Correo + '\'' +
+        return "Bibliotecario{" + super.toString() +
                 ", Salario=" + Salario +
                 ", antiguedad=" + antiguedad +
                 '}';

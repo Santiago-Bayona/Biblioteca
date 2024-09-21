2bad5f589c29edc281ff63b790446744fa14eda6
Santiago-Bayona <Cesars.bayonap@uqvirtual.edu.co>
2024-09-21 18:52:01 -0500

    Estudiente.java

--- Estudiente.java
+++ Estudiente.java
@@ -1,57 +1,15 @@
 package Biblioteca;
 
-public class Estudiente {
-    private String nombre;
-    private String cedula;
-    private String telefono;
-    private String Correo;
+
+public class Estudiente extends Persona{
 
     public Estudiente (String nombre,String cedula, String telefono,String correo){
-        this.nombre=nombre;
-        this.cedula=cedula;
-        this.telefono=telefono;
-        this.Correo=correo;
+        super(nombre,cedula,telefono,correo);
     }
 
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getCedula() {
-        return cedula;
-    }
-
-    public void setCedula(String cedula) {
-        this.cedula = cedula;
-    }
-
-    public String getTelefono() {
-        return telefono;
-    }
-
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
-    }
-
-    public String getCorreo() {
-        return Correo;
-    }
-
-    public void setCorreo(String correo) {
-        Correo = correo;
-    }
 
     @Override
     public String toString() {
-        return "Estudiente{" +
-                "nombre='" + nombre + '\'' +
-                ", cedula='" + cedula + '\'' +
-                ", telefono='" + telefono + '\'' +
-                ", Correo='" + Correo + '\'' +
-                '}';
+        return "Estudiante= {" + super.toString() +" }";
     }
 }

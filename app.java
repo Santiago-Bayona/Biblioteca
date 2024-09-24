2f5d757b83fca323f2d25075654987d8dfc43f4c
Andres-eng728 <andresm.zapatal@uqvirtual.edu.co>
2024-09-23 20:03:40 -0500

    Update app.java

--- app.java
+++ app.java
@@ -9,7 +9,7 @@ public class app {
         Estudiente estudiente1= new Estudiente("luis","147","22121212","Luis@gmail.com");
         Estudiente estudiente2 = new Estudiente("Luna","10245","3115545644", "khskshsjk@gmail.com");
         Libro libro1= new Libro("1478","14897","gabo","Cien años de Soledad","Norma", LocalDate.of(1998,4,7),7, Libro.Estado.regular);
-        Libro libroNuevo= new Libro("7885","44554","Kafka","Metamorofois","Norma", LocalDate.of(1974,5,12),7, Libro.Estado.Muy_Bien);
+        LLibro libro2= new Libro("1479","14898","aritoteles","el principito","Norma", LocalDate.of(1998,4,7),7, Libro.Estado.regular);
         Prestamo prestamo1 = new Prestamo("1787",LocalDate.of(2024,8,10),LocalDate.of(2024,8,15),5.6);
         SantoDomingo.agregarLibro(libro1);
         SantoDomingo.agregarPrestamo(prestamo1);
@@ -17,6 +17,8 @@ public class app {
         SantoDomingo.agregarEstudiante(estudiente1);
         SantoDomingo.agregarEstudiante(estudiente2);
         SantoDomingo.agregarBibliotecario(bibliotecario1);
+        Libro nuevoLibro= new Libro("1478","14898","gabo","el pricipito-edicion especial","Norma", LocalDate.of(1998,4,7),7, Libro.Estado.regular);
+        SantoDomingo.reemplazarLibro("14898",nuevoLibro);
 
         SantoDomingo.ConsultaLibro("1478");
         System.out.println(SantoDomingo);

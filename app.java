a017843d5993b796279ac954d37e4770375d4ffa
Andres-eng728 <andresm.zapatal@uqvirtual.edu.co>
2024-09-23 20:04:12 -0500

    Update app.java

--- app.java
+++ app.java
@@ -9,7 +9,7 @@ public class app {
         Estudiente estudiente1= new Estudiente("luis","147","22121212","Luis@gmail.com");
         Estudiente estudiente2 = new Estudiente("Luna","10245","3115545644", "khskshsjk@gmail.com");
         Libro libro1= new Libro("1478","14897","gabo","Cien años de Soledad","Norma", LocalDate.of(1998,4,7),7, Libro.Estado.regular);
-        LLibro libro2= new Libro("1479","14898","aritoteles","el principito","Norma", LocalDate.of(1998,4,7),7, Libro.Estado.regular);
+        Libro libro2= new Libro("1479","14898","aritoteles","el principito","Norma", LocalDate.of(1998,4,7),7, Libro.Estado.regular);
         Prestamo prestamo1 = new Prestamo("1787",LocalDate.of(2024,8,10),LocalDate.of(2024,8,15),5.6);
         SantoDomingo.agregarLibro(libro1);
         SantoDomingo.agregarPrestamo(prestamo1);

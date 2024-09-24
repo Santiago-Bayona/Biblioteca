907c00bca43c5b59c061354151531f2ab6d0a1fc
Andres-eng728 <andresm.zapatal@uqvirtual.edu.co>
2024-09-23 21:24:13 -0500

    Update Estudiente.java

--- Estudiente.java
+++ Estudiente.java
@@ -3,13 +3,26 @@ package Biblioteca;
 
 public class Estudiente extends Persona{
 
-    public Estudiente (String nombre,String cedula, String telefono,String correo){
+   public String correoinstitucional;
+    public Estudiante (String nombre,String cedula, String telefono,String correo,String correoinstitucional) {
         super(nombre,cedula,telefono,correo);
+        this.correoinstitucional = correoinstitucional;
     }
+
+    public String getCorreoinstitucional() {
+        return correoinstitucional;
+    }
+
+    public void setCorreoinstitucional(String correoinstitucional) {
+        this.correoinstitucional=correoinstitucional;
+    }
+
 
 
     @Override
     public String toString() {
-        return "Estudiante= {" + super.toString() +" }";
+        return "Estudiante= {" + super.toString() +"correoinstitucional= "+correoinstitucional+"}";
     }
+
+
 }

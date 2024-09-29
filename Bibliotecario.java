05a33f98b9e49db7a09df33f0acdf8db90ecba12
Andres-eng728 <andresm.zapatal@uqvirtual.edu.co>
2024-09-29 14:34:00 -0500

    Update Bibliotecario.java

--- Bibliotecario.java
+++ Bibliotecario.java
@@ -7,12 +7,14 @@ public class Bibliotecario extends Persona {
     private int Salario;
     private int antiguedad;
     private Collection<Prestamo>prestamos;
+    private int PrestamosRealizados;
 
     public Bibliotecario(String nombre, String cedula, String telefono, String correo, int salario, int antiguedad) {
         super(nombre, cedula, telefono, correo);
         this.Salario = salario;
         this.antiguedad = antiguedad;
         this.prestamos=new LinkedList<>();
+        this.PrestamosRealizados=0;
     }
 
 
@@ -30,6 +32,13 @@ public class Bibliotecario extends Persona {
 
     public void setAntiguedad(int antiguedad) {
         this.antiguedad = antiguedad;
+    }
+     public void incrementarPrestamos() {
+        this.PrestamosRealizados++;
+    }
+
+    public int getPrestamosRealizados() {
+        return PrestamosRealizados;
     }
 
     @Override

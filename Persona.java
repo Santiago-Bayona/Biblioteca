359816c4af40569249a4a0dd8399504fc7a7b291
Santiago-Bayona <Cesars.bayonap@uqvirtual.edu.co>
2024-10-01 19:17:01 -0500

    Add files via upload

--- Persona.java
+++ Persona.java
@@ -6,6 +6,13 @@ public class Persona {
     private String telefono;
     private String Correo;
 
+    /**
+     * Metodo constructor de la clase Persona
+     * @param nombre
+     * @param cedula
+     * @param telefono
+     * @param correo
+     */
     public Persona(String nombre,String cedula, String telefono,String correo) {
         this.nombre = nombre;
         this.cedula = cedula;
@@ -13,34 +20,66 @@ public class Persona {
         this.Correo = correo;
     }
 
+    /**
+     * Metodo que permite obtener el nombre de una persona
+     * @return
+     */
     public String getNombre() {
         return nombre;
     }
 
+    /**
+     * Metodo que permite modificar el nombre de una persona
+     * @param nombre
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
+    /**
+     * Metodo que permite obtener la cedula de una persona
+     * @return
+     */
     public String getCedula() {
         return cedula;
     }
 
+    /**
+     * Metodo que permita modificar la cedula de una persona
+     * @param cedula
+     */
     public void setCedula(String cedula) {
         this.cedula = cedula;
     }
 
+    /**
+     * Metodo que permite obtener el telefono de una persona
+     * @return
+     */
     public String getTelefono() {
         return telefono;
     }
 
+    /**
+     * Metodo que permite modificar el telefono de una persona
+     * @param telefono
+     */
     public void setTelefono(String telefono) {
         this.telefono = telefono;
     }
 
+    /**
+     * Metodo que obtener el correo de una persona
+     * @return
+     */
     public String getCorreo() {
         return Correo;
     }
 
+    /**
+     * Metodo que permite modificar el correo de una persona
+     * @param correo
+     */
     public void setCorreo(String correo) {
         Correo = correo;
     }

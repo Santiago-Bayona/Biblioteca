cb43d8ba85fb1927daf37369e55e7ce90cf3f2ee
Andres-eng728 <andresm.zapatal@uqvirtual.edu.co>
2024-10-01 19:27:57 -0500

    Update Estudiente.java

--- Estudiente.java
+++ Estudiente.java
@@ -7,17 +7,30 @@ import java.util.LinkedList;
 public class Estudiente extends Persona{
     private int edad;
     private Collection<Prestamo>prestamos;
-
+/**
+ * Metdo constructor de la clase Estudiente
+ * @param nombre
+ * @param cedula
+ * @param telefono
+ * @param correo
+ * @param edad
+ */
     public Estudiente (String nombre,String cedula, String telefono,String correo, int edad){
         super(nombre,cedula,telefono,correo);
         this.edad=edad;
         this.prestamos=new LinkedList<>();
     }
-
+/**
+ * Metodo que obtiene la edad
+ * @return
+ */
     public int getEdad() {
         return edad;
     }
-
+/**
+ * Metodo que modifica la edad
+ * @param edad
+ */
     public void setEdad(int edad) {
         this.edad = edad;
     }
